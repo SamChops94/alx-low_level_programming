e509bb3780f34275dd2bd4fb0b2b6696596177c6
SamChops94 <samchops63@gmail.com>
2022-10-13 17:02:45 -0700

    Another C program that list properties

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main-this is the entry point of the program.
- * Return:zero if no errors-nin zero value if errors
+ *
+ * Return:zero if no errors-nin zero value if errors.
 */
 
 int main(void)
@@ -10,6 +11,6 @@ int main(void)
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of long long int: %i byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %i byte(s)\n". sizeof(float));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
 	return (0);
 }
