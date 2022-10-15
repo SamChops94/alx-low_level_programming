da9026eb44ea24a7011de02dfa4d5c980e10fb7f
SamChops94 <samchops63@gmail.com>
2022-10-15 08:22:03 -0700

    the && operator

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,23 +10,23 @@
 int main(void)
 {
 	int n;
-	int ldigit;
+	int x;
 
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	ldigit = n % 10;
-	if (digit > 5)
+	x = n % 10;
+	if (x > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n ldigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n x);
 	}
-	else if (ldigit == 0)
+	else if (x == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, ldigit);
+		printf("Last digit of %d is %d and is 0\n", n, x);
 	}
-	else if (ldigit < 6 && ldigit != 0)
+	else if (x < 6 && x != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ldigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 	}
 	return (0);
 }
