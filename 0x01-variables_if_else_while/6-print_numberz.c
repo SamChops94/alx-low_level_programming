b2603e7a1b5423cb7125dc63a70c81dce39f52d5
SamChops94 <samchops63@gmail.com>
2022-10-15 15:55:22 -0700

    sixth

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio>
+
+/**
+ * main-program entry point.
+ * Return:0 no errors, non zero error.
+ *
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
