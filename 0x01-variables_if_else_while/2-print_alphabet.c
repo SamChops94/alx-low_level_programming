3069dab1182cd06e7693795f4645e77af125c4c9
SamChops94 <samchops63@gmail.com>
2022-10-15 14:31:34 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,11 @@
-#include <stdlib.h>
-
-#include <time.h>
-
 #include <stdio.h>
 
 /**
  * main-program entry point.
  *
  * Return:0 if no error, non-zero value if error
-*/
+**/
+
 int main(void)
 {
 	char i;
