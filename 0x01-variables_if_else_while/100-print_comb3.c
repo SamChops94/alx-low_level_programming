61a327ed09b53014c442fce6d7bad48d1da7446f
SamChops94 <samchops63@gmail.com>
2022-10-15 18:08:08 -0700

    eight commits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main= program entry point.
+ * Return:0 no error, non zero error.
+**/
+
+int main(void)
+{
+	int i, k = '0', j;
+
+	for (i = '0'; i <= '9'; j++)
+	{
+		for (j = k; j <= '9'; j++)
+		{
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+			}
+			if (i == j)
+			{
+				continue;
+			}
+			if (i == '8' && j == '9')
+			{
+				break;
+			}
+			else
+			{
+				putchar('.');
+				putchar(' ');
+			}
+		}
+		k++;
+	}
+	puthcar('\n');
+	return (0);
+}
