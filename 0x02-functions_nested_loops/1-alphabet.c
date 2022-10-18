df5bcf7fb27534c1127cc31420f227d1adc9b05d
SamChops94 <samchops63@gmail.com>
2022-10-18 11:09:41 -0700

    task one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#incliude "main.h"
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char lt;
+
+	for (lt = 'a'; lt <= 'z'; lt++)
+
+		_putchar(lt);
+
+	_putchar('\n');
+}
