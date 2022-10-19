8268f0d5abdb1a111e4ee8f880f5275eaafba94f
SamChops94 <samchops63@gmail.com>
2022-10-19 02:35:36 -0700

    task five

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - checks for dign of a number
+ * @n: the char being checked
+ * Return: 1 if no error, 0 error
+ */
+
+int print_sign(int n)
+{
+	if (n > '0')
+		_putchar('+');
+
+		return (1);
+
+	else if (n == 0)
+		_putchar(0);
+
+		return (0);
+	else if (n < 0)
+		_putchar('-');
+
+		return (-1);
+}
