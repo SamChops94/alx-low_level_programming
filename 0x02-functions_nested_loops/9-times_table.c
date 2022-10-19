6014b1977d9edfff3af5dd09b4837cf5d377c9de
SamChops94 <samchops63@gmail.com>
2022-10-19 07:27:27 -0700

    task nine

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table  - prints nine times table
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int nm, mlt, prd;
+
+
+	for (nm = 0; nm <= 9 nm++)
+	{
+		_putchar('0');
+
+		for (mlt = 0 mlt <= 9 mlt++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prd = nm * mlt;
+
+			if (prd <= 9)
+
+				_putchar(' ');
+			else
+				_putchar((prd / 10) + '0');
+				_putchar((prd % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
