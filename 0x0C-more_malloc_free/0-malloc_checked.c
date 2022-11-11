e5cbeef801aa99beab8a78ab0b1e2c96ef5741f9
SamChops94 <samchops63@gmail.com>
2022-11-11 14:36:46 -0800

    concat

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * malloc_checked - allocates memory using malloc.
@@ -12,12 +11,10 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+	char *p;
 
 	p = malloc(b);
 	if (p == NULL)
-	{
 		exit(98);
-	}
 	return (p);
 }
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,51 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib>
 
 /**
- *
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of bytes.
+ * Return: pointer of the allocated memory
+ * if malloc fails, status value equals 98.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
+
+
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
+
+	if (n > ls2)
+		n = ls2;
+
+	isout = ls1 + n;
+
+	sout = malloc(isout + 1);
+
+	if (sout == NULL)
+		return (NULL);
+
+
+	for (i = 0; i < isout; i++)
+
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
+	sout[i] = '\0';
+
+	return (sout);
+}
